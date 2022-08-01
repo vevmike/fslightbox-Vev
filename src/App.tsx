32a6d1c11b60ec45291b36969e31aa6ed15eaf34
vevmike <108888734+vevmike@users.noreply.github.com>
2022-08-01 09:03:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React, {useEffect, useRef, useState, } from 'react';
+import React, { useEffect, useRef, useState, } from 'react';
 import { registerVevComponent, useSize, useEditorState } from '@vev/react';
 import FsLightbox from 'fslightbox-react';
 import "./styles.css";
-
-
-//Define the default function. Cover is the image loaded when no images has been loaded.
 
 type Props = {
   title: Images;
@@ -12,63 +9,62 @@ type Props = {
   title: coverIndex;
 };
 
-const Lightbox = ({ showCover=true, Images, coverIndex = 1 }: Props) => {
-
-
+const Lightbox = ({ showCover = true, Images, coverIndex = 1 }: Props) => {
   const cover = "https://cdn.vev.design/cdn-cgi/image/f=auto,q=82/private/eZnxAcdFstddxKYGM8AXAUiPaAo2/image/_W5nWypsaj.png";
-  const [ key,setKey ] = useState(0);
-  const [ toggler,setToggler ] = useState(false);
-  const [ imgList,setImglist ] = useState([]);
-  const [ visible,setVisible ] = useState(true);
-  const [ defaultCoveron,setDefaultcoveron ] = useState(true);
-  const [ coverURL,setCoverurl ] = useState("https://cdn.vev.design/cdn-cgi/image/f=auto,q=82/private/eZnxAcdFstddxKYGM8AXAUiPaAo2/image/_W5nWypsaj.png");
+  const [key, setKey] = useState(0);
+  const [toggler, setToggler] = useState(false);
+  const [imgList, setImglist] = useState([]);
+  const [visible, setVisible] = useState(true);
+  const [defaultCoveron, setDefaultcoveron] = useState(true);
+  const [coverURL, setCoverurl] = useState("https://cdn.vev.design/cdn-cgi/image/f=auto,q=82/private/eZnxAcdFstddxKYGM8AXAUiPaAo2/image/_W5nWypsaj.png");
   const { disabled } = useEditorState();
- 
+
   useEffect(() => {
     var a = 0
     if (typeof Images !== 'undefined') {
       let tempList = []
-      if (Images.length > 0){
-        for (var i = 0; (i <= (Images.length - 1 )); i++) {
-                  tempList.push(Images[i].image?.url)
+      if (Images.length > 0) {
+        for (var i = 0; (i <= (Images.length - 1)); i++) {
+          tempList.push(Images[i].image?.url)
         }
-      setImglist(tempList)
-      setDefaultcoveron(false)
-      setCoverurl(tempList[(coverIndex- 1)])
-        } else {
-          setDefaultcoveron(true)
-        }
+        setImglist(tempList)
+        setDefaultcoveron(false)
+        setCoverurl(tempList[(coverIndex - 1)])
+      } else {
+        setDefaultcoveron(true)
+      }
     }
-  setKey(a)
-  console.log("Images length change")
+    setKey(a)
+    console.log("Images length change")
   }, [Images?.length, defaultCoveron])
 
   useEffect(() => {
-    if (imgList.length > 0){
-      setCoverurl(imgList[(coverIndex- 1)])
+    if (imgList.length > 0) {
+      setCoverurl(imgList[(coverIndex - 1)])
       setDefaultcoveron(false)
     }
     console.log("coverindex change")
-  },[coverIndex])
+  }, [coverIndex])
 
   useEffect(() => {
     setVisible(showCover)
     console.log("visible", showCover)
-  },[showCover])
+  }, [showCover])
 
-  return (   
-  <>
-  <div className='wrapper'>
-    <img src={defaultCoveron ? cover : coverURL } style={{opacity: (visible ? 1 : 0)}} alt="Load an image" onClick={() => setToggler(!toggler)}/>
-    
-  <FsLightbox
-  toggler={toggler}
-  sources={defaultCoveron ? [cover] : coverURL}
-  thumbs={imgList}
-  key={key}
-  slide={1}/></div>
-  </>
-)};
+  return (
+    <>
+      <div className='wrapper'>
+        <img src={defaultCoveron ? cover : coverURL} style={{ opacity: (visible ? 1 : 0) }} alt="Load an image" onClick={() => setToggler(!toggler)} />
+
+        <FsLightbox
+          toggler={toggler}
+          sources={defaultCoveron ? [cover] : imgList}
+          thumbs={imgList}
+          key={key}
+          slide={1} /></div>
+    </>
+  )
+};
 
 registerVevComponent(Lightbox, {
   name: "fslightbox-Vev3",
@@ -82,20 +78,18 @@ registerVevComponent(Lightbox, {
           type: "image",
         },
       ],
-      
     },
     {
-        name: "coverIndex",
-        type: "number",
-        title: "Index of cover img:",
-        initialValue: 1,
-
+      name: "coverIndex",
+      type: "number",
+      title: "Index of cover img:",
+      initialValue: 1,
     },
     {
-        name: "showCover",
-        type: "boolean",
-        title: "Show cover image",
-        initialValue:true,
+      name: "showCover",
+      type: "boolean",
+      title: "Show cover image",
+      initialValue: true,
     },
   ],
 });
